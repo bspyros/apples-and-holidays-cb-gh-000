a36085a54c5cb73fd090fbaf415a374f45e16388
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-25 11:35:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday_hash|
+  holiday_hash.each do |season, s_holiday_hash|
     puts "#{season}:"
+
     
   end
 end
